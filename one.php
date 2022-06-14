e93bffa76cf417c85e98e5707d64163f95af90d6
krishna <sk@codelinkinfotech.com>
2022-06-14 15:00:56 +0530

    add multiplication function in one.php

--- one.php
+++ one.php
@@ -2,7 +2,7 @@
 
 echo add(7, 9);
 echo sub(9,6);
-
+echo mul(9,6);
 // add function
 function add($x, $y){
     $c = $x + $y;
@@ -12,6 +12,9 @@ function sub($x, $y){
     $c = $x - $y;
     return $c;
 }
-
+function mul($x, $y){
+    $c = $x*$y;
+    return $c;
+}
 
 ?>
