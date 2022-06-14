07ddae7e78e2e9fdba2e66d400bffecb2b009d7a
krishna <sk@codelinkinfotech.com>
2022-06-14 14:46:49 +0530

    i add subtract function in one.php

--- one.php
+++ one.php
@@ -1,11 +1,15 @@
 <?php
 
 echo add(7, 9);
-
+echo sub(9,6);
 
 // add function
 function add($x, $y){
     $c = $x + $y;
+    return $c;
+}
+function sub($x, $y){
+    $c = $x - $y;
     return $c;
 }
 
