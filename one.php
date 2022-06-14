3a4bf5722ad2d5ea9739520d10773ea515523e2f
Narendra Singh <ns@codelinkinfotech.com>
2022-06-14 12:50:03 +0530

    mul function in two.php and add comment in one.php

--- one.php
+++ one.php
@@ -1,10 +1,13 @@
 <?php
 
+echo add(7, 9);
+
+
+// add function
 function add($x, $y){
     $c = $x + $y;
     return $c;
 }
 
-echo add(7, 9);
 
 ?>
---
+++ two.php
@@ -0,0 +1,12 @@
+<?php
+
+echo mul(7, 9);
+
+//mul function
+function mul($x, $y){
+    $c = $x * $y;
+    return $c;
+}
+
+
+?>
